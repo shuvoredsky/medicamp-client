16729c0b39c8de272b4c65e937d63e8b37a6a066
shuvoredsky <kumarshuvo265@gmail.com>
2025-07-09 01:57:26 +0600

    update navbar available camps

--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -6,6 +6,7 @@ import ErrorPage from "../Pages/ErroPage/ErrorPage.jsx";
 import RootLayout from "../RootLayout/RootLayout.jsx";
 import Dashboard from "../Dashboard/Dashboard.jsx";
 import AddCamp from "../Dashboard/AddCamp.jsx";
+import AvailableCamps from "../Pages/Available-Camp/AvailableCamps.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ export const router = createBrowserRouter([
       {
         path: "/sign-up",
         Component: SignUp,
+      },
+      {
+        path: "/available-camps",
+        Component: AvailableCamps,
       },
       {
         path: "dashboard",
